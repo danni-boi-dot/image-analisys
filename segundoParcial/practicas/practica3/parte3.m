e88373807aee52cc185e51055d3c3c25577578aa
Daniel <adtdaniel87@gmail.com>
2024-04-24 17:03:51 -0600

    practica 3 terminada

--- segundoParcial/practicas/practica3/parte3.m
+++ segundoParcial/practicas/practica3/parte3.m
@@ -9,7 +9,7 @@ end
 % Generar y mostrar el histograma de la imagen original
 figure;
 histogram(img, 256, 'Normalization', 'probability');
-title('Histograma de la imagen original');
+title('Histograma');
 xlabel('Niveles de Gris');
 ylabel('Probabilidad');
 
@@ -29,14 +29,24 @@ nodes = [probabilidades' (0:255)'];
 nodes = sortrows(nodes, -1);
 
 % Guardar las probabilidades ordenadas en un archivo txt
-fid = fopen('pro.txt', 'w');
+fid = fopen('parte3.txt', 'w');
 for i = 1:256
-    fprintf(fid, 'Pixel: %d, Probabilidad: %f\n', nodes(i, 2), nodes(i, 1));
+    % Calcular el número de bits necesarios para representar el píxel
+    bits = ceil(log2(i));
+    fprintf(fid, 'Pixel: %d, Probabilidad: %f, Bits: %d\n', nodes(i, 2), nodes(i, 1), bits);
 end
 
 % Mostrar las probabilidades iniciales
 disp('Probabilidades ordenadas:');
 disp(nodes(:, 1)');
+
+% Calcular el número total de símbolos en la fuente de información
+n = numel(img);
+
+% Calcular el porcentaje de agrupamiento
+porcentaje_agrupamiento = (2/n) * (n - 1) * 100;
+
+disp(['Porcentaje de agrupamiento aproximado: ', num2str(porcentaje_agrupamiento), '%']);
 
 % Sumar las dos últimas probabilidades hasta que solo queden dos
 while size(nodes, 1) > 2
@@ -56,7 +66,9 @@ while size(nodes, 1) > 2
     % Guardar las probabilidades ordenadas en un archivo txt después de cada suma
     fprintf(fid, '\nDespués de sumar, las probabilidades ordenadas son:\n');
     for i = 1:size(nodes, 1)
-        fprintf(fid, 'Pixel: %d, Probabilidad: %f\n', nodes(i, 2), nodes(i, 1));
+        % Calcular el número de bits necesarios para representar el píxel
+        bits = ceil(log2(nodes(i, 2)));
+        fprintf(fid, 'Pixel: %d, Probabilidad: %f, Bits: %d\n', nodes(i, 2), nodes(i, 1), bits);
     end
     
     % Mostrar las probabilidades restantes
@@ -66,4 +78,18 @@ end
 
 fclose(fid);
 
-disp('Probabilidades ordenadas guardadas en "pro.txt"');
+disp('Probabilidades ordenadas guardadas en ".txt"');
+
+% Calcular la entropía de la fuente de información
+entropia = -sum(probabilidades .* log2(probabilidades));
+
+% Calcular la longitud media de los códigos de Huffman
+longitud_media = sum(probabilidades .* ceil(log2(1 ./ probabilidades)));
+
+% Calcular la eficiencia del código de Huffman
+eficiencia = entropia / longitud_media;
+
+% Convertir la eficiencia a porcentaje
+eficiencia_porcentaje = eficiencia * 100;
+
+disp(['Eficiencia del código de Huffman: ', num2str(eficiencia_porcentaje), '%']);
